d879ae506d08459284b4eb7192f97ccf2247c50a
Juan Carlos Guerrero Borunda <juancgb00@gmail.com>
2023-04-28 09:28:10 -0600

    Manejo de rutas

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app= express();
 
 app.get('/',function(req,res){
-    res.send('Hola mundo !');
+    res.send('Una API basica desdes express');
+});
+
+app.get('/saludo',function(req,res){
+    res.send('Hola desde la API');
+});
+
+app.get('/despedida',function(req,res){
+    res.send('Adios desdes una API');
 });
 
 app.listen(3000,function(){
